a00cbcb00265fd77f4ba6afba898414c6a601200
Morshed <morshed.monsterstudio@gmail.com>
2025-03-18 16:42:12 +0600

    fix: toolbar design as like figma

--- app/components/Editor/NotionToolbar/NotionToolbar.tsx
+++ app/components/Editor/NotionToolbar/NotionToolbar.tsx
@@ -1,11 +1,19 @@
 import { ToolbarRenderProps } from '@yoopta/toolbar';
 import { useYooptaEditor, useYooptaTools } from '@yoopta/editor';
 import cx from 'classnames';
-import { CodeIcon, ChevronDownIcon, RadicalIcon } from 'lucide-react';
+import { ChevronDownIcon } from 'lucide-react';
 import { useState } from 'react';
 import { useFloating, offset, flip, shift, inline, autoUpdate, FloatingPortal } from '@floating-ui/react';
 import s from './NotionToolbar.module.scss';
 import { buildActionMenuRenderProps } from '@yoopta/action-menu-list';
+import CodeSvg from './icons/CodeSvg';
+import UndoSvg from './icons/UndoSvg';
+import RedoSvg from './icons/RedoSvg';
+import BoldSvg from './icons/BoldSvg';
+import ItalicSvg from './icons/ItalicSvg';
+import UnderlineSvg from './icons/UnderlineSvg';
+import StrikeSvg from './icons/StrikeSvg';
+import TextAlignSvg from './icons/TextAlignSvg';
 
 const DEFAULT_MODALS = { link: false, highlight: false, actionMenu: false };
 type ModalsState = typeof DEFAULT_MODALS;
@@ -46,28 +54,7 @@ const NotionToolbar = (props: ToolbarRenderProps) => {
     <div className='font-sans'>
       <div className={s.toolbar}>
         <div className={s.group}>
-          <div className="flex justify-center mb-2">
-            <button
-              type="button"
-              onClick={() => {
-                editor.redo({ scroll: false });
-              }}
-              className="p-2 text-xs shadow-md border-r hover:bg-[#64748b] hover:text-[#fff] disabled:opacity-50"
-              disabled={editor.historyStack.redos.length === 0}
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-redo-2"><path d="m15 14 5-5-5-5" /><path d="M20 9H9.5A5.5 5.5 0 0 0 4 14.5A5.5 5.5 0 0 0 9.5 20H13" /></svg>
-            </button>
-            <button
-              type="button"
-              onClick={() => {
-                editor.undo({ scroll: false });
-              }}
-              className="p-2 text-xs shadow-md hover:bg-[#64748b] hover:text-[#fff] disabled:opacity-50"
-              disabled={editor.historyStack.undos.length === 0}
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-undo-2"><path d="M9 14 4 9l5-5" /><path d="M4 9h10.5a5.5 5.5 0 0 1 5.5 5.5a5.5 5.5 0 0 1-5.5 5.5H11" /></svg>
-            </button>
-          </div>
+
         </div>
         <div className={s.group}>
           <button
@@ -77,7 +64,7 @@ const NotionToolbar = (props: ToolbarRenderProps) => {
             onClick={() => onChangeModal('actionMenu', !modals.actionMenu)}
           >
             <span className={s.block}>
-              {blockLabel} <ChevronDownIcon size={12} strokeWidth={2} color="rgba(55, 53, 47, 0.35)" />
+              {blockLabel} <ChevronDownIcon size={12} strokeWidth={2} />
             </span>
             {modals.actionMenu && !!ActionMenu && (
               <FloatingPortal id="yoo-custom-toolbar-action-menu-list-portal" root={editor.refElement}>
@@ -88,34 +75,66 @@ const NotionToolbar = (props: ToolbarRenderProps) => {
             )}
           </button>
         </div>
+        {/* this is for bold/italic/underline */}
         <div className={s.group}>
+          {/* this is for undo/redo */}
+          <div className="flex justify-center">
+            <button
+              type="button"
+              onClick={() => {
+                editor.redo({ scroll: false });
+              }}
+              className="p-2 text-xs shadow-md border-r hover:bg-[rgba(0,0,0,0.05)] hover:text-[#fff] disabled:opacity-50"
+              disabled={editor.historyStack.redos.length === 0}
+            >
+              <RedoSvg />
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                editor.undo({ scroll: false });
+              }}
+              className="p-2 text-xs shadow-md hover:bg-[rgba(0,0,0,0.05)] hover:text-[#fff] disabled:opacity-50"
+              disabled={editor.historyStack.undos.length === 0}
+            >
+              <UndoSvg />
+            </button>
+          </div>
           <button
             type="button"
             onClick={() => editor.formats.bold.toggle()}
             className={cx(s.item, { [s.active]: editor.formats.bold.isActive() })}
           >
-            <span className={s.bold}>B</span>
+            <span className={s.bold}>
+              <BoldSvg />
+            </span>
           </button>
           <button
             type="button"
             onClick={() => editor.formats.italic.toggle()}
             className={cx(s.item, { [s.active]: editor.formats.italic.isActive() })}
           >
-            <span className={s.italic}>i</span>
+            <span className={s.italic}>
+              <ItalicSvg />
+            </span>
           </button>
           <button
             type="button"
             onClick={() => editor.formats.underline.toggle()}
             className={cx(s.item, { [s.active]: editor.formats.underline.isActive() })}
           >
-            <span className={s.underline}>U</span>
+            <span className={s.underline}>
+              <UnderlineSvg />
+            </span>
           </button>
           <button
             type="button"
             onClick={() => editor.formats.strike.toggle()}
             className={cx(s.item, { [s.active]: editor.formats.strike.isActive() })}
           >
-            <span className={s.strikethrough}>S</span>
+            <span className={s.bold}>
+              <StrikeSvg />
+            </span>
           </button>
           <button
             type="button"
@@ -123,19 +142,20 @@ const NotionToolbar = (props: ToolbarRenderProps) => {
             className={cx(s.item, { [s.active]: editor.formats.code.isActive() })}
           >
             <span className={s.code}>
-              <CodeIcon size={15} strokeWidth={1.5} />
+              <CodeSvg />
             </span>
           </button>
-          {/* <button
+        </div>
+        <div className='s.group'>
+          <button
             type="button"
-            // onClick={() => alert('See example with custom text format')}
-            onClick={() => editor.formats.katex.toggle()}
-            className={cx(s.item, { [s.active]: editor.formats.katex.isActive() })}
+            onClick={() => editor.formats.align.toggle()}
+            className={cx(s.item, { [s.active]: editor.formats.code.isActive() })}
           >
             <span className={s.code}>
-              <RadicalIcon size={15} strokeWidth={1.5} />
+              <TextAlignSvg />
             </span>
-          </button> */}
+          </button>
         </div>
       </div>
     </div>
