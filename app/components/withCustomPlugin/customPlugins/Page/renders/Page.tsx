c622f923f52e80eeb140e83b8dee811a55d09dac
sajibms <sajib.intern@gmail.com>
2025-03-19 16:50:10 +0600

    fixing child page error

--- app/components/withCustomPlugin/customPlugins/Page/renders/Page.tsx
+++ app/components/withCustomPlugin/customPlugins/Page/renders/Page.tsx
@@ -1,79 +1,81 @@
 import { useEffect, useState } from "react";
-import { Elements, PluginElementRenderProps, useYooptaEditor } from "@yoopta/editor";
-import { Link, useFetcher } from "@remix-run/react";
+import {
+  Elements,
+  PluginElementRenderProps,
+  useYooptaEditor,
+} from "@yoopta/editor";
+import {
+  Form,
+  Link,
+  useActionData,
+  useFetcher,
+  useNavigate,
+} from "@remix-run/react";
 
 const PageRenderElement = ({
   element,
   attributes,
-  blockId
+  blockId,
 }: PluginElementRenderProps) => {
-  const fetcher = useFetcher();
-  console.log(fetcher,'fetcther');
+  // const fetcher = useFetcher();
+  // console.log(fetcher, "fetcther");
+  const actionData = useActionData();
   const editor = useYooptaEditor();
-  const [pageId,setPageId] = useState();
+  const navigate = useNavigate();
   console.log("PageRenderElement", element);
   // console.log("PageRenderEditor", editor.children);
   // console.log("PageRenderEditor", editor);
 
   // Listen for API responses via fetcher.
   useEffect(() => {
-    if (fetcher?.data?.id && fetcher?.data?.title) {
-      // Update the element props with the new page ID and title.
-      const newProps = {
-        ...element.props,
-        pageId: fetcher.data.id, // Update the pageId with the new ID.
-        title: fetcher.data.title, // Update the title with the new title.
-      };
-      setPageId(fetcher?.data?.id)
-      // console.log(newProps);
-      const elementPath = Elements.getElementPath(editor, blockId, element);
+    const handleFetcherData = async () => {
+      if (actionData?.id && actionData?.title) {
+        const elementPath = Elements.getElementPath(editor, blockId, element);
 
-      Elements.updateElement(
-              editor,
-              blockId,
-              {type:'page',props: { ...element.props,pageId:fetcher?.data?.id }},
-              { path: elementPath },
+        Elements.updateElement(
+          editor,
+          blockId,
+          {
+            type: "page",
+            props: { ...element.props, pageId: actionData?.id },
+          },
+          { path: elementPath }
         );
 
-      // Update the element with the new props.
-      // Transforms.setNodes(editor, { props: newProps }, { at: element.path });
-    }
-  }, [fetcher.data]);
-  console.log(pageId,'Page Id');
+        // * wait for a  second to allow the editor to update
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
+        // if (element.props.pageId) {
+        //   navigate(`/dashboard/content/${actionData?.id}`, { replace: true });
+        // }
+      }
+    };
+
+    handleFetcherData();
+  }, [actionData]);
+
   return (
     <div
       {...attributes}
       className="w-full flex relative items-center h-[20px] my-1"
       contentEditable={false}
     >
-      {pageId ? (
+      {actionData ? (
         <Link
-          to={`/dashboard/content/${pageId}`}
+          to={`/dashboard/content/${actionData?.id}`}
           className="text-blue-500 hover:underline px-3 py-2 border-2 rounded-sm"
         >
           {element.props.title}
         </Link>
       ) : (
-        <button
-          onClick={(e) => {
-            e.preventDefault()
-            e.stopPropagation()
-            // Trigger the API call to create the page.
-            // This submits the form data, including parentId if available.
-            fetcher.submit(
-              {
-                title: element.props.title,
-              },
-              { method: "post", action: "/api/new-page" }
-            );
-          }}
-          type="button"
-          className="text-blue-500 hover:underline px-3 py-2 border-2 rounded-sm"
-        >
-          {fetcher.state === "submitting"
-            ? "Creating page..."
-            : "Create New Page"}
-        </button>
+        <Form method="post">
+          <button
+            type="submit"
+            className="text-blue-500 hover:underline px-3 py-2 border-2 rounded-sm"
+          >
+            Create New Page
+          </button>
+        </Form>
       )}
     </div>
   );
