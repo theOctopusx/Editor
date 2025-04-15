9ed865d377142588fa5159234eeaa0224b6bbe7e
coder7475 <robiulhossain7475@gmail.com>
2025-04-15 10:44:24 +0600

    feat: undo cahnges in pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,14 +3,6 @@ module.exports = {
     {
       name: 'editor',
       script: 'pnpm start',
-      instances: "max",
-      exec_mode: "cluster",
-      // Important for Remix in cluster mode
-      wait_ready: true,
-      listen_timeout: 10000,
-      kill_timeout: 5000,
-      // This helps prevent memory leaks
-      max_memory_restart: '500M'
     },
   ],
 }
